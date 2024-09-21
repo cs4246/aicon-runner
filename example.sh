80eeac35a74ff86db05444c0cb9d3ec4ff4bee94
Muhammad Rizki Aulia Rahman Maulana <rizki@rizkiarm.com>
2024-09-22 03:45:03 +0800

    feat: simplify example.sh

--- example.sh
+++ example.sh
@@ -2,17 +2,14 @@
 
 now=$(date)
 
-echo "---"
-echo "$now"
-
 PIDFILE="$HOME/aicon-runner.pid"
 
 if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= | ps -p $(cat ${PIDFILE}) > /dev/null); then
-  echo "aicon-runner is already running."
-  exit 99
+  echo "$now: aicon-runner is already running."
+  exit
 fi
 
-echo "starting aicon-runner..."
+echo "$now: starting aicon-runner..."
 cd $HOME/aicon-runner/
 . .venv/bin/activate
 python -m aicon_runner --concurrency 5 & echo $! > "${PIDFILE}"
